bbedd34d6767ee96e0243046c7204f2a45d0abad
Mark Hannah <hannahm7@msn.com>
2015-05-11 12:49:14 -0400

    Added S3 for paperclip

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -80,4 +80,14 @@ Rails.application.configure do
   #Required for Heroku
   config.action_mailer.default_url_options = { host: 'icuriovideos.herokuapp.com' }
 
+  #sets paperclip to uplaod images to s3
+  config.paperclip_defaults = {
+    :storage => :s3,
+    :s3_credentials => {
+      :bucket => ENV['icuriovideos'],
+      :access_key_id => ENV['AWS_ACCESS_KEY_ID'],
+      :secret_access_key => ENV['AWS_SECRET_ACCESS_KEY']
+  }
+}
+
 end
