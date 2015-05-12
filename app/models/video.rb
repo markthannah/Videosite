a089a45c4e29797ec7c90e9501b7ed7420e9b81c
Mark Hannah <hannahm7@msn.com>
2015-05-12 13:12:25 -0400

    Updated index and show pages

--- app/models/video.rb
+++ app/models/video.rb
@@ -1,5 +1,5 @@
 class Video < ActiveRecord::Base
   belongs_to :user
-  has_attached_file :image, :styles => { :medium => "300x300>", :thumb => "100x100>" }
+  has_attached_file :image, :styles => { :medium => "400x400>", :thumb => "100x100>" }
   do_not_validate_attachment_file_type :image
 end
