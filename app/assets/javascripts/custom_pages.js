e9f2b75921706c5af32cc4a7dcc06ab5ef6a4330
Mark Hannah <hannahm7@msn.com>
2015-07-12 21:23:09 -0400

    Added page breaks to CP descriptions and changed video player

--- app/assets/javascripts/custom_pages.js
+++ app/assets/javascripts/custom_pages.js
@@ -2,16 +2,3 @@
 # All this logic will automatically be available in application.js.
 # You can use CoffeeScript in this file: http://coffeescript.org/
 /*Requieres jQuery*/
-$('#myModal').bind('hide', function () {
- var iframe = $(this).children('div.modal-body').find('iframe');
- var src = iframe.attr('src');
- iframe.attr('src', '');
- iframe.attr('src', src);
-});
-
-$(‘.btnFullscreen’).on(‘click’, 
-function() {
-videoPlayer[0].webkitEnterFullscreen();
-videoPlayer[0].mozRequestFullScreen();
-return false;
-});
